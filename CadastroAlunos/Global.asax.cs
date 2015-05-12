21741faad65f7ec11d6f0902fc7e2e570725de48
Eduardo <eduardo30.machado@hotmail.com>
2015-05-12 20:05:15 -0300

    Teste WebConfig

--- CadastroAlunos/Global.asax.cs
+++ CadastroAlunos/Global.asax.cs
@@ -17,7 +17,7 @@ namespace CadastroAlunos
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
+			
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
